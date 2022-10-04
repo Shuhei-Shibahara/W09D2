a7b43a7da7bfe914bdf5226702cf1a5987150fb0
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-04 15:52:23 -0700

    finished bind events

--- ticTacToe/src/index.js
+++ ticTacToe/src/index.js
@@ -1,6 +1,12 @@
-const View = // require appropriate file
-const Game = // require appropriate file
+const View = require("./ttt-view.js");// require appropriate file
+// const Game = // require appropriate file
+
+// const game = new Game();
+const view = new View();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
+  const posses = document.getElementById('ttt')
+  view.setupBoard();
+  view.bindEvents();
 });
--- ticTacToe/src/ttt-view.js
+++ ticTacToe/src/ttt-view.js
@@ -1,14 +1,54 @@
+const Game = require('../ttt_node/game.js')
+
 class View {
-  constructor(game, el) {}
+  constructor(game, el) {
 
-  setupBoard() {}
+  }
+
+  setupBoard() {
+    let figure = document.querySelector('.ttt');
+    console.log(figure)
+    let ul = document.createElement('ul');
+    figure.appendChild(ul);
+    
+    
+    for(let i = 0; i < 3; i++){
+      for (let j = 0; j < 3; j++){
+        let li = document.createElement('li');
+        li.setAttribute('data-pos',`[${i},${j}]`);
+        ul.appendChild(li);
+      }
+    }
+  }
+  handleClick(e) {
+    let mark = 'X';
+    let pos = e.target.attributes['data-pos'].value
+
+    e.target.style.backgroundColor = 'lightblue'
+   
+    if (e.target.attributes['class'] === undefined){
+      e.target.setAttribute('class', mark) 
+    }
+    debugger
+    this.makeMove(pos);
+  }
   
-  bindEvents() {}
+  bindEvents() {
+    let ul = document.querySelector('ul');
+    ul.addEventListener('click', this.handleClick);
+  }
+  
+  
+  makeMove(square) {
 
-  handleClick(e) {}
 
-  makeMove(square) {}
-
+  }
+  
 }
 
 module.exports = View;
+
+// const addMove = (e) => {
+//   e.preventDefault;
+//   let input = document.querySelector('li')
+// }
