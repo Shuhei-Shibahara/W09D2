250dec885048cbba8cc5b98f964491815cf9ec0a
Shuhei Shibahara <Shoehey06@gmail.com>
2022-10-04 17:04:26 -0700

    finished tic tac toe

--- ticTacToe/src/index.js
+++ ticTacToe/src/index.js
@@ -1,12 +1,12 @@
 const View = require("./ttt-view.js");// require appropriate file
-// const Game = // require appropriate file
+const Game = require('../ttt_node/game.js') // require appropriate file
 
-// const game = new Game();
-const view = new View();
 
 document.addEventListener("DOMContentLoaded", () => {
   // Your code here
   const posses = document.getElementById('ttt')
+  const game = new Game();
+  const view = new View(game, posses);
   view.setupBoard();
   view.bindEvents();
 });
--- ticTacToe/src/ttt-view.js
+++ ticTacToe/src/ttt-view.js
@@ -1,8 +1,10 @@
-const Game = require('../ttt_node/game.js')
+const Game = require('../ttt_node/game.js');
 
 class View {
   constructor(game, el) {
-
+    this.handleClick = this.handleClick.bind(this)
+    this.game = game
+    this.el = el
   }
 
   setupBoard() {
@@ -21,15 +23,21 @@ class View {
     }
   }
   handleClick(e) {
-    let mark = 'X';
-    let pos = e.target.attributes['data-pos'].value
-
+    let mark = this.game.currentPlayer;
+    console.log(mark)
+    let pos = e.target.getAttribute('data-pos');
+    let posX = parseInt(pos[1]);
+    let posY = parseInt(pos[3]);
     e.target.style.backgroundColor = 'lightblue'
-   
+    
     if (e.target.attributes['class'] === undefined){
       e.target.setAttribute('class', mark) 
+      this.game.playMove([posX,posY]);
+      e.target.innerHTML = mark;
+    }else{
+      alert ("Hey fck face, wrong move");
     }
-    debugger
+    
     this.makeMove(pos);
   }
   
@@ -41,6 +49,32 @@ class View {
   
   makeMove(square) {
 
+    if (this.game.isOver() === true){
+      if(this.game.winner() === null){
+        let p = document.querySelector('.result')
+        p.innerHTML = 'Draw is as good as a lose!'
+      }else{
+        let mark = this.game.currentPlayer;
+        if (mark === 'o'){
+          let p = document.querySelector('.result')
+          p.innerHTML = `Well Done X`
+          let winners = document.querySelectorAll("[class = 'x']")
+          // debugger
+          winners.forEach(winner => {
+            winner.style.backgroundColor = 'lightgreen'
+          })
+        }else{
+          let p = document.querySelector('.result')
+          p.innerHTML = 'Well Done O'
+          let winners = document.querySelectorAll("[class = 'o']")
+          winners.forEach(winner => {
+            winner.style.backgroundColor = 'lightgreen'
+          })
+        }
+      }
+      
+      // let winner = document.getElementsByClassName('.X')
+    }
 
   }
   
